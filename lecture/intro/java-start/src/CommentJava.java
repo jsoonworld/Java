b9231e019bdc97637cd914eb96adaab36dce7056
권장순 <jsoonworld@gmail.com>
2024-03-11 23:11:20 +0900

    feat: variable 실습 코드 정리.

---
+++ lecture/intro/java-start/src/CommentJava.java
@@ -0,0 +1,7 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력한다.
+//        System.out.println("hello java2"); // hello java2을 출력한다.
+    }
+}
---
+++ lecture/intro/java-start/src/variable/ex/VarEx10Question.java
@@ -0,0 +1,9 @@
+package variable.ex;
+
+public class VarEx10Question {
+    public static void main(String[] args) {
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+}
